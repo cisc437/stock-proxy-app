95f990157872f47e870170460b9e798d02fb12ff
Prof Ninja Novocin <andy@novocin.com>
2015-02-26 11:32:08 +0000

    comments added

--- api.php
+++ api.php
@@ -1,25 +1,50 @@
 <?php
+//This script demonstrates several things:
+//1) an API need not be complicated, a one of file can do the job IF you have 
+//   a very limited scope for the project.  Extending this would not be pleasant
 
-$sample_ticker = $_GET["ticker"];
+//2) Proxy requests: unlike the BGG app, this script will hit the 3rd party in 
+//   real time.  I'm using curl which is a standard way to make programmatic 
+//   http requests.
 
+//This is a faster way to get URL parameters, sorry I didn't think of it sooner
+$ticker = $_GET["ticker"];
+
+//If you don't use my API correctly you get this message
 if (!isset($_GET["ticker"])){
     die("usage: ?ticker=XXXX");
 }
 
+//these lines took some time to make.  I went to the YQL console made a command
+//that I liked and looked at the URL that was displayed in a box at the bottom 
+//of the screen.  The part that was missing was env=blahblahblah.  That is 
+//what told YQL where to find the yahoo.finance.quote table.
+
+//YQL is mostly just SQL with extra url parameters.
 $yql_base_url = "https://query.yahooapis.com/v1/public/yql";
-$yql_query = "select * from yahoo.finance.quote where symbol=\"".$sample_ticker."\"";
+$yql_query = "select * from yahoo.finance.quote where symbol=\"".$ticker."\"";
 $the_url = $yql_base_url."?q=".urlencode($yql_query)."&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 
+//Please NOTE that when running SQL commands the above lines would be open to 
+//SQL injection.  We are sending our commands through a set of url paramters, 
+//so the task of fighting the injection is on Yahoo in this one case only.
+
+//These lines are not my own work, they are google searched using terms like 
+// PHP Curl GET Example
 $curl_handler = curl_init();
 
 curl_setopt($curl_handler, CURLOPT_URL, $the_url);
 curl_setopt($curl_handler, CURLOPT_RETURNTRANSFER, true);
 
 $output = curl_exec($curl_handler);
-$robj = json_decode($output);
 
+//This is mine, I "decode" the JSON object into a PHP object and return to my
+//clients the stock quote.  This is not checking even checking for error.
+//This whole thing is quick and dirty to get the job done.
+$robj = json_decode($output);
 print_r(json_encode($robj->query->results->quote));
 
+//close curl out.
 curl_close($curl_handler);
 
 ?>
--- ng-stock.js
+++ ng-stock.js
@@ -1,12 +1,22 @@
+//name the module, no dependencies (overall)
 var myApp = angular.module('stockApp', []);
 
+//my controller on the other hand wants data-binding and http requests
+//those get passed in.
 myApp.controller('stockController', ['$scope', '$http', function($scope, $http) {
+
+  //I have value="GOOG" in my HTML but the ng-model cleared it, so this line 
+  //gives me a default value of GOOG.
   $scope.ticker = "GOOG";
+  
+  //this was so that my ng-repeat wasn't working on an undefined variable.
   $scope.query = {};
 
   $scope.loadData = function(){
+    //this uses my single-file API just as I ask it too.
     $http.get("api.php?ticker="+$scope.ticker).success(
       function(data, status, headers, config){
+        //the response is not expecting anything in particular.
         $scope.query = data;
       });
   };
